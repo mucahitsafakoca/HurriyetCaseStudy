58456c39f69448be47ae71e7efc17412fe38c0f2
mucahitsafakoca <124301701+mucahitsafakoca@users.noreply.github.com>
2025-11-12 17:05:01 +0300

    Update Driver.java

--- src/test/java/com/web/base/driver/Driver.java
+++ src/test/java/com/web/base/driver/Driver.java
@@ -31,14 +31,21 @@ public class Driver {
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--disable-gpu");
+            options.addArguments("--disable-extensions");
             options.addArguments("--window-size=1920,1080");
+            options.addArguments("--remote-debugging-port=9222");
+            options.addArguments("--disable-software-rasterizer");
+            options.addArguments("--disable-dev-tools");
+            options.addArguments("--disable-translate");
+            options.addArguments("--disable-background-networking");
+            options.addArguments("--mute-audio");
         }
 
         webDriver = new ChromeDriver(options);
+        webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
 
         webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
-        webDriverWait
-                .pollingEvery(Duration.ofMillis(500))
+        webDriverWait.pollingEvery(Duration.ofMillis(500))
                 .ignoring(NoSuchElementException.class)
                 .ignoring(StaleElementReferenceException.class);
 
